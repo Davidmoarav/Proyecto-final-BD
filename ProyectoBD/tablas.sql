e090e81f2ce7d59d8e6b31ba58c114fe996d808b
David Moya Aravena <91227158+Davidmoarav@users.noreply.github.com>
2024-11-19 09:06:27 -0300

    Create tablas.sql

---
+++ ProyectoBD/tablas.sql
@@ -0,0 +1,8 @@
+--TABLA CLIENTES
+CREATE TABLE DJNJ_CLIENTE(
+    COD_CLIENTE NUMBER,
+    nombre_cliente VARCHAR2(50),
+    apellido1_cliente VARCHAR2(50),
+    apellido2_cliente VARCHAR2(50),
+    correo_cliente VARCHAR2(30)
+);
